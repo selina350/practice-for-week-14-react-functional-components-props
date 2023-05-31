f70f2cb9cbb1604311f4d4510986b9f974a494b2
Selina Shi <109104101+selina350@users.noreply.github.com>
2023-05-31 20:56:49 +0000

    Update 2 files

---
+++ src/BaseStats.js
@@ -0,0 +1,11 @@
+import "./BaseStats.css";
+
+const BaseStats = (props) => {
+  return (
+    <div className="baseStats">
+      <h1>BaseStats</h1>
+    </div>
+  );
+};
+
+export default BaseStats;
